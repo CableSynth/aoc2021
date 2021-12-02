7897327cff29e728f1ff35e0e7707c2be52d98e2
CableSynth <lorencalebbaum@gmail.com>
2021-12-02 14:22:46 -0500

    trying to understand the functional stuff better

--- src/main.rs
+++ src/main.rs
@@ -1,58 +1,42 @@
 use std::fs;
 
-fn solve_first(input: &str) -> u64 {
-    input
+fn solve_first(input: &str) -> i64 {
+    let (depth, width) = input
         .lines()
-        .map(|x| x.parse().unwrap())
-        .fold((0, i32::MAX), |(sum, previous), number| {
-            (if number > previous { sum + 1 } else { sum }, number)
+        .filter_map(|direction_map| {
+            direction_map.split_once(' ')
+            .map(|(direction, distance)| (direction, distance.parse::<i64>().unwrap()))
         })
-        .0
+        .fold((0, 0), |(depth, horizontal), (direction, distance)| match direction {
+            "forward" => (depth, horizontal + distance),
+            "down" => (depth + distance, horizontal),
+            "up" => (depth - distance, horizontal),
+            &_ => (depth, horizontal)
+        });
+    depth * width
+}
+
+fn solve_second(input: &str) -> i64 {
+    let (depth, width, _) = input
+        .lines()
+        .filter_map(|direction_map| {
+            direction_map.split_once(' ')
+            .map(|(direction, distance)| (direction, distance.parse::<i64>().unwrap()))
+        })
+        .fold((0, 0, 0), |(depth, horizontal, aim), (direction, distance)| match direction {
+            "forward" => (depth + aim * distance, horizontal + distance, aim),
+            "down" => (depth, horizontal, aim + distance),
+            "up" => (depth, horizontal, aim - distance),
+            &_ => (depth, horizontal, aim)
+        });
+    depth * width
 }
 
 fn main() {
-    let filename = "src/input.txt";
-    let data = fs::read_to_string(filename).expect("error");
-    let data_split_on_newline: Vec<&str> = data.split("\n").collect();
-    let mut total = 0;
-    let mut previous = data_split_on_newline[0].parse::<i64>().expect("Bad Value");
-    for line in data_split_on_newline {
-        let compline = line.parse::<i64>().expect("Bad Value");
-        if compline > previous {
-            total += 1;
-        }
-        previous = compline;
-    }
-    println!("{}", total);
-
-    let data_split_on_newline: Vec<&str> = data.split("\n").collect();
-    total = 0;
-    previous = 0;
-    for i in 0..(data_split_on_newline.len()) {
-        let compline = data_split_on_newline[i].parse::<i64>().expect("Bad Value");
-        let compline1 = data_split_on_newline[i + 1]
-            .parse::<i64>()
-            .expect("Bad Value");
-        let compline2 = data_split_on_newline[i + 2]
-            .parse::<i64>()
-            .expect("Bad Value");
-        let sum = compline + compline1 + compline2;
-        // println!("{}", sum);
-        if previous == 0 {
-            previous = sum;
-            continue;
-        }
-        if sum > previous {
-            total += 1;
-        }
-        previous = sum;
-        if i + 3 == data_split_on_newline.len() {
-            break;
-        }
-    }
-    println!("{}", total);
 
     let input = include_str!("input.txt");
     let solution1 = solve_first(input);
+    let solution2 = solve_second(input);
     println!("{}", solution1);
+    println!("{}", solution2);
 }
